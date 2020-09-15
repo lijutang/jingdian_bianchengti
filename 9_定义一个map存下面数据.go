19948f5db20fcd8f95d07c26b784cf0d01b9dbb5
ljt <742641068@qq.com>
2020-09-15 08:21:54 +0800

    定义一个map存下面数据

--- 9_定义一个map存下面数据.go
+++ 9_定义一个map存下面数据.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 /*
 	1）定义一个map存下面数据
 	France 首都是 巴黎
@@ -19,4 +24,27 @@ func main() {
 	countryCapitalMap["Italy"] = "罗马"
 	countryCapitalMap["Japan"] = "东京"
 	countryCapitalMap["India "] = "新德里"
+	//使用键输出地图值
+	for country := range countryCapitalMap {
+		fmt.Println(country,"首都是",countryCapitalMap[country])
+	}
+	//查看元素在集合中是否存在
+	capital,ok := countryCapitalMap["American"]//如果确定是真实的,则存在,否则不存在
+	if ok {
+		fmt.Println("American的首都是",capital)
+	} else {
+		fmt.Println("merican的首都不存在")
+	}
+	//用户名：密码＠tcp(地址：3306)/是数据库名
+	db,err := sql.Open("mysql","root:123456@tcp(192.168.1.15)/test")
+	if err != nil {
+		fmt.Println(err)
+	}
+	//网数据库中插如数据
+	for k,v := range countryCapitalMap {
+		result,err := db.Exec("INSERT  INTO countryCapital(country,capital)values(?,?) ",k,v)
+		if err != nil {
+			fmt.Println(result,err)
+		}
+	}
 }
